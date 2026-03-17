179266dc82ccfbd499e84194a287a141dd97f496
Appiah Paul Olives <submissions.moonrisestories@gmail.com>
2026-03-17 01:09:18 -0700

    create novel page

---
+++ app/novel/page.tsx
@@ -0,0 +1,8 @@
+export default function NovelPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Explore Stories</h1>
+      <p>All novels will appear here soon 🚀</p>
+    </main>
+  );
+}
